228b76b21b9c905b0659a51b2a90bd051a11c5cd
Joseph S Lookabaugh <josephlooka@gmail.com>
2019-06-02 15:27:45 -0400

    Add test for passing in only 1 param

--- kata_test.py
+++ kata_test.py
@@ -6,13 +6,17 @@ class testCalAdd(unittest.TestCase):
     Basic cal test class
     """
 
-    def test_twoNumbers(self):
+    def test_twoStrings(self):
         res = calAdd('1','2')
         self.assertEqual(res, 3)
 
     def test_emptyString(self):
 		res = calAdd('','2')
 		self.assertEqual(res, 2)
+		
+    def test_oneString(self):
+		res = calAdd('1')
+		self.assertEqual(res, 1)
 		
 if __name__ == '__main__':
     unittest.main()
