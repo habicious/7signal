e29c0f28d371d7ef96f385c8991d650f2be9626a
Joseph S Lookabaugh <josephlooka@gmail.com>
2019-06-02 15:03:36 -0400

    Initial commit with first test

---
+++ kata_test.py
@@ -0,0 +1,15 @@
+import unittest
+from kata import calAdd
+
+class testCalAdd(unittest.TestCase):
+    """
+    Basic cal test class
+    """
+
+    def test_twoNumbers(self):
+        res = calAdd('1','2')
+        self.assertEqual(res, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
