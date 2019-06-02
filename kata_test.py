f46c1213a5eab7d86f7e4b1fea5ea7d20af10598
Joseph S Lookabaugh <josephlooka@gmail.com>
2019-06-02 15:19:05 -0400

    Testing for passing in an empty string, also updating EOL chars to unix

--- kata_test.py
+++ kata_test.py
@@ -10,6 +10,9 @@ class testCalAdd(unittest.TestCase):
         res = calAdd('1','2')
         self.assertEqual(res, 3)
 
+    def test_emptyString(self):
+		res = calAdd('','2')
+		self.assertEqual(res, 2)
 		
 if __name__ == '__main__':
     unittest.main()
