d63f68ea91df5568f062cea34d57c7dfd7d0a446
Joseph S Lookabaugh <josephlooka@gmail.com>
2019-06-02 16:41:28 -0400

    Allow for variable delimiters

--- kata.py
+++ kata.py
@@ -2,10 +2,16 @@ import sys
 
 def calAdd(numString):
     total = 0
-
-    numString = numString.replace('\n', ',')
-    numInput = [x.strip() for x in numString.split(',')]
+    delim = ','
 	
+    isDelim = numString[:2]
+    if isDelim == '//':
+        delim = numString[2:3]
+        numString = numString.split('\n')[1]
+
+    numString = numString.replace('\n', delim)
+    numInput = [x.strip() for x in numString.split(delim)]
+
     for num in numInput:
 		total = total + int(num.strip() or 0)
 
--- kata_test.py
+++ kata_test.py
@@ -26,6 +26,10 @@ class testCalAdd(unittest.TestCase):
 		res = calAdd('1\n2,3')
 		self.assertEqual(res, 6)
     
-		
+    def test_variousDelims(self):
+		res = calAdd('//;\n1;2')
+		self.assertEqual(res, 3)
+
+
 if __name__ == '__main__':
     unittest.main()
