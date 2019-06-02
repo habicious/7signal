482729f66036ce198db6294b93e0d566dfd0fd0f
Joseph S Lookabaugh <josephlooka@gmail.com>
2019-06-02 15:49:15 -0400

    All treating newlines as delimiters

--- kata.py
+++ kata.py
@@ -3,6 +3,7 @@ import sys
 def calAdd(numString):
     total = 0
 
+    numString = numString.replace('\n', ',')
     numInput = [x.strip() for x in numString.split(',')]
 	
     for num in numInput:
